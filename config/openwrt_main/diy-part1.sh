9c2298c6813d67766d30c4d867572f3354c49754
Biaogo94 <44315262+Biaogo94@users.noreply.github.com>
2025-12-06 00:38:48 +0800

    Update diy-part1.sh

--- config/openwrt_main/diy-part1.sh
+++ config/openwrt_main/diy-part1.sh
@@ -13,23 +13,18 @@
 # rm -rf package/utils/{ucode,fbtest}
 
 #!/bin/bash
-# ... (保留原有的注释)
-
-# 1. 添加 Passwall 源 (依赖和本体)
+# 必装: Passwall
 echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall;packages" >> feeds.conf.default
 echo "src-git passwall_luci https://github.com/xiaorouji/openwrt-passwall;luci" >> feeds.conf.default
 
-# 2. 添加 OpenClash (直接克隆源码到 package 目录)
+# 必装: OpenClash
 git clone --depth 1 https://github.com/vernesong/OpenClash.git package/luci-app-openclash
 
-# 3. 添加 Turbo ACC 网络加速
+# 必装: TurboACC
 git clone --depth 1 https://github.com/chenmozhijian/luci-app-turboacc.git package/luci-app-turboacc
 
-# 4. 添加 Tailscale 界面 (官方源通常只有核心 core，没有界面)
+# 必装: Tailscale (界面)
 git clone --depth 1 https://github.com/asvow/luci-app-tailscale.git package/luci-app-tailscale
 
-# 5. 确保 ZeroTier 存在 (官方源通常已有，但为了保险可以添加一个常用库，或者直接依赖官方 feed)
-# 官方 OpenWrt feed 通常包含 luci-app-zerotier，无需额外添加
-
-# 6. 晶晨盒子插件 (luci-app-amlogic)
-# 注意：你的 diy-part2.sh 中已经包含了 luci-app-amlogic 的下载命令，这里不用重复。
+# 可选: Argon 主题 (N1 常用主题)
+git clone --depth 1 https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
